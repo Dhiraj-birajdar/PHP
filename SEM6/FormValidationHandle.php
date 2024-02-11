98e8c77334354d9f13ba7816ec6b0843ffd7b68a
Dhiraj-birajdar <birajdar257@gmail.com>
2024-02-11 20:29:02 +0530

    php form validation

--- SEM6/FormValidationHandle.php
+++ SEM6/FormValidationHandle.php
@@ -1,21 +1,24 @@
 <?php
+function sanitize($data){
+    return htmlspecialchars(stripslashes(trim($data))) ;
+}
 echo "<h3>";
 if (isset($_POST['ok'])) {
 
     if (!empty($_POST["name"])) {
-        echo "Name: " . $_POST['name'] . "<br>";
+        echo "Name: " . sanitize($_POST['name']) . "<br>";
     } else {
         echo "Name is missing <br>";
     }
 
     if (!empty($_POST["username"]) and strlen($_POST['username']) >= 5) {
-        echo "UserName: " . $_POST['username'] . "<br>";
+        echo "UserName: " . sanitize($_POST['username']) . "<br>";
     } else {
         echo "UserName should be atleast 5 characters <br>";
     }
 
     if (!empty($_POST["pass"]) and strlen($_POST['pass']) >= 5) {
-        echo "Password: " . $_POST['pass'] . "<br>";
+        echo "Password: " . sanitize($_POST['pass']) . "<br>";
     } else {
         echo "Password should be atleast 5 characters <br>";
     }
